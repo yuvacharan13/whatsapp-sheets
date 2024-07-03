d1804cb34eeb48f0c83c33bef26052b44811b890
yuvacharan13 <57321298+yuvacharan13@users.noreply.github.com>
2024-07-03 13:19:51 +0530

    First commit - added index page

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.send("Webhook received");
+});
+
+app.get("/yuva", (req, res) => {
+  res.send("hey yuva path");
+});
+
+app.get("/", (req, res) => {
+  res.send("hey yuva home page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
